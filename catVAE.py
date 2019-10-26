8c1a1fffd28e58de3db4431ab94d05e8030327b0
Ritwik Bera <ritwikbera12@gmail.com>
2019-10-26 15:45:13 -0500

    added own catVAE template

--- catVAE.py
+++ catVAE.py
@@ -1,63 +1,28 @@
-# Code to implement VAE-gumple_softmax in pytorch
-# author: Devinder Kumar (devinder.kumar@uwaterloo.ca), modified by Yongfei Yan
-# The code has been modified from pytorch example vae code and inspired by the origianl \
-# tensorflow implementation of gumble-softmax by Eric Jang.
-
-import argparse
 import numpy as np
-
 import torch
-import torch.nn.functional as F
 from torch import nn, optim
 from torch.nn import functional as F
-from torchvision import datasets, transforms
-from torchvision.utils import save_image
 
-parser = argparse.ArgumentParser(description='VAE MNIST Example')
-parser.add_argument('--batch-size', type=int, default=100, metavar='N',
-                    help='input batch size for training (default: 100)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--temp', type=float, default=1.0, metavar='S',
-                    help='tau(temperature) (default: 1.0)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--hard', action='store_true', default=False,
-                    help='hard Gumbel softmax')
+latent_dim = 1
+categorical_dim = 2  # one-of-K vector
+temp = 1.0
+temp_min = 0.5
+ANNEAL_RATE = 0.00003
+epochs = 10
+seed = 1
+batch_size = 1
+hard = False
 
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-
-torch.manual_seed(args.seed)
-if args.cuda:
-    torch.cuda.manual_seed(args.seed)
-
-kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-train_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('./data', train=True, download=True,
-                   transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('./data', train=False, transform=transforms.ToTensor()),
-    batch_size=args.batch_size, shuffle=True, **kwargs)
-
+torch.manual_seed(seed)
 
 def sample_gumbel(shape, eps=1e-20):
     U = torch.rand(shape)
-    if args.cuda:
-        U = U.cuda()
     return -torch.log(-torch.log(U + eps) + eps)
 
 
 def gumbel_softmax_sample(logits, temperature):
     y = logits + sample_gumbel(logits.size())
     return F.softmax(y / temperature, dim=-1)
-
 
 def gumbel_softmax(logits, temperature, hard=False):
     y = gumbel_softmax_sample(logits, temperature)
@@ -70,10 +35,8 @@ def gumbel_softmax(logits, temperature, hard=False):
     y_hard = torch.zeros_like(y).view(-1, shape[-1])
     y_hard.scatter_(1, ind.view(-1, 1), 1)
     y_hard = y_hard.view(*shape)
-    # Set gradients w.r.t. y_hard gradients w.r.t. y
     y_hard = (y_hard - y).detach() + y
     return y_hard.view(-1, latent_dim * categorical_dim)
-
 
 class VAE_gumbel(nn.Module):
     def __init__(self, temp):
@@ -106,16 +69,7 @@ class VAE_gumbel(nn.Module):
         z = gumbel_softmax(q_y, temp, hard)
         return self.decode(z), F.softmax(q_y, dim=-1).reshape(*q.size())
 
-
-latent_dim = 30
-categorical_dim = 10  # one-of-K vector
-
-temp_min = 0.5
-ANNEAL_RATE = 0.00003
-
-model = VAE_gumbel(args.temp)
-if args.cuda:
-    model.cuda()
+model = VAE_gumbel(temp)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
@@ -132,10 +86,7 @@ def loss_function(recon_x, x, qy):
 def train(epoch):
     model.train()
     train_loss = 0
-    temp = args.temp
     for batch_idx, (data, _) in enumerate(train_loader):
-        if args.cuda:
-            data = data.cuda()
         optimizer.zero_grad()
         recon_batch, qy = model(data, temp, args.hard)
         loss = loss_function(recon_batch, data, qy)
@@ -144,55 +95,13 @@ def train(epoch):
         optimizer.step()
         if batch_idx % 100 == 1:
             temp = np.maximum(temp * np.exp(-ANNEAL_RATE * batch_idx), temp_min)
-
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader),
-                       loss.item()))
-
+    
     print('====> Epoch: {} Average loss: {:.4f}'.format(
         epoch, train_loss / len(train_loader.dataset)))
 
-
-def test(epoch):
-    model.eval()
-    test_loss = 0
-    temp = args.temp
-    for i, (data, _) in enumerate(test_loader):
-        if args.cuda:
-            data = data.cuda()
-        recon_batch, qy = model(data, temp, args.hard)
-        test_loss += loss_function(recon_batch, data, qy).item() * len(data)
-        if i % 100 == 1:
-            temp = np.maximum(temp * np.exp(-ANNEAL_RATE * i), temp_min)
-        if i == 0:
-            n = min(data.size(0), 8)
-            comparison = torch.cat([data[:n],
-                                    recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
-            save_image(comparison.data.cpu(),
-                       'data/reconstruction_' + str(epoch) + '.png', nrow=n)
-
-    test_loss /= len(test_loader.dataset)
-    print('====> Test set loss: {:.4f}'.format(test_loss))
-
-
 def run():
-    for epoch in range(1, args.epochs + 1):
+    for epoch in range(1, epochs + 1):
         train(epoch)
-        test(epoch)
-
-        M = 64 * latent_dim
-        np_y = np.zeros((M, categorical_dim), dtype=np.float32)
-        np_y[range(M), np.random.choice(categorical_dim, M)] = 1
-        np_y = np.reshape(np_y, [M // latent_dim, latent_dim, categorical_dim])
-        sample = torch.from_numpy(np_y).view(M // latent_dim, latent_dim * categorical_dim)
-        if args.cuda:
-            sample = sample.cuda()
-        sample = model.decode(sample).cpu()
-        save_image(sample.data.view(M // latent_dim, 1, 28, 28),
-                   'data/sample_' + str(epoch) + '.png')
-
 
 if __name__ == '__main__':
     run()
