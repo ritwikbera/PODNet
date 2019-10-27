d5cde68d273556b57d157c838c23a1636674dad8
Ritwik Bera <ritwikbera12@gmail.com>
2019-10-27 12:22:58 -0500

    finished model (no errors)

--- catVAE.py
+++ catVAE.py
@@ -6,7 +6,7 @@ from torch.nn import functional as F
 state_dim = 3
 action_dim = 3
 latent_dim = 2 #has a similar effect to multi-head attention
-categorical_dim = 2 #number of options to be discovered 
+categorical_dim = 3 #number of options to be discovered 
 
 #from Directed-InfoGAIL
 temp = 5.0
@@ -15,16 +15,22 @@ ANNEAL_RATE = 0.003
 learning_rate = 0.0003
 mlp_hidden = 64
 
-epochs = 10
+epochs = 50
 seed = 1   #required for random gumbel sampling
-batch_size = 1
 hard = False  
 
 torch.manual_seed(seed)
 
 #randomly generated trajectory data
 traj_length = 100
-traj_data = torch.randn(traj_length, state_dim + action_dim)
+traj_num = 1 #similar to minibatch size (how many traj to be simultaneously processed)
+traj_data = torch.randn(traj_length, traj_num, state_dim + action_dim)
+
+def gen_traj_data(traj_length):
+    # #drawing a circle
+    # r = 10
+    # theta = 0
+    pass
 
 def sample_gumbel(shape, eps=1e-20):
     U = torch.rand(shape)
@@ -77,19 +83,19 @@ class PODNet(nn.Module):
         return self.relu(self.fc3(h2))
         
     def decode_next_state(self, s_t, c_t):
-        z = torch.cat((s_t, c_t), 1)
+        z = torch.cat((s_t, c_t), -1)
         h1 = self.relu(self.fc4(z))
         h2 = self.relu(self.fc5(h1))
         return self.relu(self.fc6(h2))
 
     def decode_action(self, s_t, c_t):
-        z = torch.cat((s_t, c_t), 1)
+        z = torch.cat((s_t, c_t), -1)
         h1 = self.relu(self.fc7(z))
         h2 = self.relu(self.fc8(h1))
         return self.relu(self.fc9(h2))
 
     def forward(self, s_t, c_prev, temp, hard):
-        x = torch.cat((s_t, c_prev), 1)
+        x = torch.cat((s_t, c_prev), -1)
         q = self.encode(x.view(-1, state_dim + latent_dim*categorical_dim))
         q_y = q.view(q.size(0), latent_dim, categorical_dim)
         c_t = gumbel_softmax(q_y, temp, hard)
@@ -106,7 +112,7 @@ def loss_function(next_state_pred, true_next_state, action_pred, true_action, c_
     loss_fn = torch.nn.MSELoss(reduction='sum')
     MSE = Lambda_1 * loss_fn(next_state_pred, true_next_state) + Lambda_2 * loss_fn(action_pred, true_action)
 
-    q_y = c_t
+    qy = c_t
     log_ratio = torch.log(qy * categorical_dim + 1e-20)
     KLD = torch.sum(qy * log_ratio, dim=-1).mean()
 
@@ -116,24 +122,35 @@ def loss_function(next_state_pred, true_next_state, action_pred, true_action, c_
 def train(epoch):
     model.train()
     train_loss = 0
+    global temp, epochs 
     #c_t_initial = torch.eye(latent_dim,option_dim)
     c_t_initial =  torch.Tensor([[1,0,0],[1,0,0]])
-    c_t_stored = c_t_initial.view(1,latent_dim*option_dim)
+    c_t_stored = c_t_initial.view(-1, 1, latent_dim*categorical_dim)
 
-    for i, data in enumerate(traj_data):
-        state, action = data[:state_dim], data[state_dim:]
+    i=0
+    for data in traj_data:
+        state, action = data[:,:state_dim], data[:,state_dim:]
+        i += 1
+        # print(i)
+        # print('state: ')
+        # print(state)
+        # print(' action:')
+        # print(action)
         optimizer.zero_grad()
         c_prev = c_t_stored[-1]
         action_pred, next_state_pred, c_t = model(state, c_prev, temp, hard)
-        c_t_stored = torch.cat((c_t_stored, c_t),0)
+        c_t_stored = torch.cat((c_t_stored, c_t.unsqueeze(0)),0)
         loss = loss_function(next_state_pred, state, action_pred, action, c_t)
-        loss.backward()
+        loss.backward(retain_graph=True)
         train_loss += loss.item()
         optimizer.step()
         
     temp = np.maximum(temp * np.exp(-ANNEAL_RATE*epoch), temp_min)
     print('====> Epoch: {} Average loss: {:.4f}'.format(
         epoch, train_loss / len(traj_data)))
+
+    if epoch == epochs:
+        print(c_t_stored)
 
 def run():
     for epoch in range(1, epochs + 1):
