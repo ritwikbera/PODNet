0590e9ff3ed6000faa345f83995ce56d5ba239f2
Ritwik Bera <ritwikbera12@gmail.com>
2019-11-12 11:21:43 -0600

    added recurrent option encoder

--- podnet.py
+++ podnet.py
@@ -18,6 +18,24 @@ from utils import to_categorical, normalize, denormalize
 
 import pickle
 
+use_recurrent = True
+
+class OptionEncoder(nn.Module):
+    def __init__(self, output_size, input_size, hidden_layer_size=100, num_layers=2):
+        super().__init__()
+        self.hidden_layer_size = hidden_layer_size
+        self.lstm = nn.LSTM(input_size, hidden_layer_size, num_layers)
+        self.linear = nn.Linear(hidden_layer_size, output_size)
+        self.hidden_cell = (torch.zeros(num_layers,1,self.hidden_layer_size),
+                            torch.zeros(num_layers,1,self.hidden_layer_size))
+
+    def forward(self, state, c_prev):
+        c_prev = c_prev.view(c_prev.size(0), -1)
+        state = state.view(state.size(0), -1)
+        lstm_out, self.hidden_cell = self.lstm(torch.cat((state,c_prev),-1).unsqueeze(0), 
+            (self.hidden_cell[0].detach(), self.hidden_cell[1].detach()))
+        predictions = self.linear(lstm_out.view(1,1,-1))
+        return predictions.squeeze(0)  #single length input so squeeze out seq_len dimension
 
 class PODNet(nn.Module):
     def __init__(self, state_dim, action_dim, latent_dim, categorical_dim, mlp_hidden=32):
@@ -29,10 +47,14 @@ class PODNet(nn.Module):
         self.latent_dim = latent_dim
         self.categorical_dim = categorical_dim
 
-        #option inference layers
-        self.fc1 = nn.Linear(state_dim + latent_dim*categorical_dim, mlp_hidden)
-        self.fc2 = nn.Linear(mlp_hidden, mlp_hidden)        
-        self.fc3 = nn.Linear(mlp_hidden, latent_dim * categorical_dim)
+        if use_recurrent:
+            self.OptInference = OptionEncoder(output_size=latent_dim*categorical_dim, 
+                input_size=state_dim+latent_dim*categorical_dim)
+        else:
+            #option inference layers
+            self.fc1 = nn.Linear(state_dim + latent_dim*categorical_dim, mlp_hidden)
+            self.fc2 = nn.Linear(mlp_hidden, mlp_hidden)        
+            self.fc3 = nn.Linear(mlp_hidden, latent_dim * categorical_dim)
 
         #policy network layers
         # the *2 terms accounts for c_prev
@@ -54,6 +76,9 @@ class PODNet(nn.Module):
         self.use_dropout_encode = False
         self.use_dropout_dynamics = False
         self.use_dropout_policy = False
+
+    def recurrent_encode(self, s_t, c_prev):
+        return self.OptInference(s_t, c_prev)
 
     def encode(self, x):
         h1 = self.relu(self.fc1(x))
@@ -85,8 +110,11 @@ class PODNet(nn.Module):
         return self.fc9(h2)
 
     def forward(self, s_t, c_prev, temp, hard):
-        x = torch.cat((s_t, c_prev), -1)
-        q = self.encode(x.view(-1, self.state_dim + self.latent_dim*self.categorical_dim))
+        if use_recurrent:
+            q = recurrent_encode(s_t, c_prev)
+        else:
+            x = torch.cat((s_t, c_prev), -1)
+            q = self.encode(x.view(-1, self.state_dim + self.latent_dim*self.categorical_dim))
         q_y = q.view(q.size(0), self.latent_dim, self.categorical_dim)
         c_t = gumbel_softmax(q_y, temp, self.latent_dim, self.categorical_dim, hard)
 
