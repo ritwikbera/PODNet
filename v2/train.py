a36779fd3fa4eceb39ce8b757466f46eae49ff19
Ritwik Bera <ritwikbera12@gmail.com>
2019-11-18 23:22:12 -0600

    added segment batch loader template

--- v2/train.py
+++ v2/train.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import numpy as np 
 import time 
 import argparse
+import pandas as pd 
 
 from netv2 import *
 
@@ -16,21 +17,38 @@ args = parser.parse_args()
 torch.manual_seed(100)
 
 PAD_TOKEN = 0
-SEGMENT_SIZE = 128
-BATCH_SIZE = 1
 STATE_DIM = 2
 
 action_dim = 2
 latent_dim = 1
 categorical_dim = 3
 
-max_length = 6000
-num_segments = max_length // SEGMENT_SIZE + 1
+SEGMENT_SIZE = 256
+TRAJ_BATCH_SIZE = 1
+MAX_LENGTH = SEGMENT_SIZE*4  #set any integer multiple of SEGMENT_SIZE
+BATCH_SIZE = TRAJ_BATCH_SIZE*MAX_LENGTH // SEGMENT_SIZE 
+num_trajectories = 1
+num_batches = num_trajectories/TRAJ_BATCH_SIZE
+    
+def load_segment_batch(i):
 
-if args.dummy_test:
-    num_segments = 1
-    cur_state_segment = torch.ones(BATCH_SIZE, SEGMENT_SIZE, STATE_DIM)
-    next_state_segment = torch.ones(BATCH_SIZE, SEGMENT_SIZE, STATE_DIM)
+    # trajectories is a python list of variable length tensors (trajectories)
+    traj_batch = trajectories[TRAJ_BATCH_SIZE*i:TRAJ_BATCH_SIZE*(i+1)]
+
+    traj = Tensor([PAD_TOKEN]).repeat(TRAJ_BATCH_SIZE, MAX_LENGTH, STATE_DIM)
+    traj_ns = traj
+    
+    for i, trajectory in enumerate(traj_batch):
+        traj[i][:trajectory.size(0)] = trajectory
+        traj_ns[i][:trajectory.size(0)-1] = trajectory[1:]   
+
+    traj = torch.split(traj, SEGMENT_SIZE, dim=-2) #split along length dimension
+    traj = torch.stack(traj).view(-1, SEGMENT_SIZE, STATE_DIM)
+    
+    traj_ns = torch.split(traj_ns, SEGMENT_SIZE, dim=-2) #split along length dimension
+    traj_ns = torch.stack(traj_ns).view(-1, SEGMENT_SIZE, STATE_DIM)
+
+    return traj, traj_ns
 
 model = PODNet(
     state_dim=STATE_DIM, 
@@ -40,7 +58,7 @@ model = PODNet(
 
 optimizer = optim.Adam(model.parameters(), lr=args.lr)
 
-def train(cur_state_segment, next_state_segment):
+def train_step(cur_state_segment, next_state_segment):
     next_state_pred, c_t = model(cur_state_segment)
     mask = (next_state_segment!=PAD_TOKEN).type(torch.FloatTensor)
     loss = (((next_state_segment - next_state_pred)**2)*mask).sum(-1).sum(-2).mean(0)
@@ -51,8 +69,19 @@ def train(cur_state_segment, next_state_segment):
     loss.backward()
     optimizer.step()
 
-for i in range(args.epochs):
-    for j in range(num_segments):
-        train(cur_state_segment, next_state_segment)
+def train(device):
+    print('On device: {}'.format(device))
 
-#if __name__ =='__main__':
+    if args.dummy_test:
+        num_batches = 1
+        BATCH_SIZE = 1
+        cur_state_segment = torch.ones(BATCH_SIZE, SEGMENT_SIZE, STATE_DIM)
+        next_state_segment = torch.ones(BATCH_SIZE, SEGMENT_SIZE, STATE_DIM)
+
+    for i in range(args.epochs):
+        for j in range(num_batches):
+            #cur_state_segment, next_state_segment = load_segment_batch(j)
+            train_step(cur_state_segment, next_state_segment)
+
+
+train(torch.device('cpu'))
