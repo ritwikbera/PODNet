b005bdd5b906d59beef8916708b2ab5db391ce0a
Ritwik Bera <ritwikbera12@gmail.com>
2019-11-26 21:20:14 -0600

    added visualizations

--- model_check.py
+++ model_check.py
@@ -1,42 +1,107 @@
 import torch
 from torch import Tensor, nn
 from models import *
+from config import *
 from utils import RoboDataset
 from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+import os
 
-PAD_TOKEN = -99
-MAX_LENGTH = 512
+PAD_TOKEN = -99.0
 dataset = 'circleworld'
+
+conf = config(dataset)
 
 my_dataset = RoboDataset(
     PAD_TOKEN=PAD_TOKEN, 
-    MAX_LENGTH=MAX_LENGTH, 
+    MAX_LENGTH=conf.MAX_LENGTH, 
     root_dir='data/'+dataset+'/')
 
 dataloader = DataLoader(my_dataset, batch_size=1,
                     shuffle=True, num_workers=1)
 
-#states = torch.rand(1,492,2)
+filename = 'mylogs/checkpoints/checkpoint_model_1000.pth'
 
-states, _, actions = next(iter(dataloader))
 
-model = PODNet(
-    batch_size=states.size(0),
-    state_dim=states.size(-1),
-    action_dim=actions.size(-1),
-    latent_dim=1,
-    categorical_dim=2,
-    encoder_type='recurrent',
-    device='cpu')
+def load_model(filename, conf, enc_type='attentive'):
+    model = PODNet(
+        batch_size=conf.batch_size,
+        state_dim=conf.state_dim,
+        action_dim=conf.action_dim,
+        latent_dim=conf.latent_dim,
+        categorical_dim=conf.categorical_dim,
+        encoder_type=enc_type,
+        device='cpu')
 
-filename = 'mylogs/checkpoints/checkpoint_model_100.pth'
-model.load_state_dict(torch.load(filename))
-model.eval()
-print('Saved Model Loaded')
+    model.load_state_dict(torch.load(filename, map_location=torch.device('cpu')))
+    model.eval()
+    print('Saved Model Loaded')
 
-def hook_fn(m, i, o):
-    print('Option Inference Logits {}'.format(o))
-model.infer_option.linear.register_forward_hook(hook_fn)
+    return model
 
-out = model(states)
-print(model.decode_next_state)
+podnet = load_model(filename, conf, enc_type='attentive')
+
+#plot first trajectory in the acquired batch
+def plot_podnet(batch, index_within_batch):
+    batch, i = batch, index_within_batch
+
+    iterator = iter(dataloader)
+
+    for batch in range(batch+1):
+        states, true_next_states, actions = next(iterator)
+
+    action_pred, next_state_pred, c_t = podnet(states, tau=0.1)
+
+
+    padded_state = np.repeat([PAD_TOKEN], states.size(-1))
+
+
+    states = states[i].detach().numpy()
+    true_next_states = true_next_states[i].detach().numpy()
+    actions = actions[i].detach().numpy()
+    next_state_pred = next_state_pred[i].detach().numpy()
+    action_pred = action_pred[i].detach().numpy()    
+    c_t = c_t[i].detach().numpy()
+
+
+    stop_index = 0
+    for index in range(len(true_next_states)):
+        if np.array_equal(true_next_states[index], padded_state):
+            stop_index = index
+            break
+
+    time = np.arange(0,stop_index,10)
+
+    # plot
+    os.makedirs('plots', exist_ok=True)
+    plt.figure()
+    plt.plot(true_next_states[:stop_index,0], 'b-', label='Truth')
+    plt.plot(true_next_states[:stop_index,1], 'r-')
+    plt.plot(next_state_pred[:stop_index,0], 'b--', label='Predicted')
+    plt.plot(next_state_pred[:stop_index,1], 'r--')
+    plt.xlabel('Time Steps')
+    plt.ylabel('Position')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('plots/dynamics.png', dpi=300)
+    plt.show()
+
+    plt.figure()
+    c_t = c_t[0:stop_index:10]
+    plt.plot(time, np.argmax(c_t, axis=-1), 'ro')
+    plt.xlabel('Time Steps')
+    plt.ylabel('Option')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('plots/options.png', dpi=300)
+    plt.show()
+
+plot_podnet(0,0)
+
+
+# def hook_fn(m, i, o):
+#     print('Option Inference Logits {}'.format(o))
+# model.infer_option.linear.register_forward_hook(hook_fn)
+
+# out = model(states)
+# print(model.decode_next_state)
