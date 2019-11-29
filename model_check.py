9b4b41ef38a2bcaaff040d2173faad34e654e3df
viniciusguigo <viniciusguigo@gmail.com>
2019-11-29 10:18:58 -0600

    modified plotting script to take max number of steps to be plotted

--- model_check.py
+++ model_check.py
@@ -12,6 +12,7 @@ parser = ArgumentParser()
 parser.add_argument('--dataset', type=str, default='robotarium')
 parser.add_argument('--encoder_type', type=str, default='attentive')
 parser.add_argument('--filename', type=str, default='checkpoint_model_23.pth')
+parser.add_argument('--max_steps', type=int, default=None)
 args = parser.parse_args()
 
 PAD_TOKEN = -99.0
@@ -47,7 +48,7 @@ def load_model(filename, conf, enc_type):
 podnet = load_model(filename, conf, enc_type=args.encoder_type)
 
 #plot first trajectory in the acquired batch
-def plot_podnet(batch, index_within_batch):
+def plot_podnet(batch, index_within_batch, max_steps):
     batch, i = batch, index_within_batch
 
     iterator = iter(dataloader)
@@ -72,12 +73,18 @@ def plot_podnet(batch, index_within_batch):
     action_pred = action_pred[i].detach().numpy()    
     c_t = c_t[i].detach().numpy()
 
+    # check if want to plot all steps or just a given number
+    if max_steps is None:
+        stop_index = 0
+        for index in range(len(true_next_states)):
+            if np.array_equal(true_next_states[index], padded_state):
+                stop_index = index
+                break
+    else:
+        stop_index = max_steps
 
-    stop_index = 0
-    for index in range(len(true_next_states)):
-        if np.array_equal(true_next_states[index], padded_state):
-            stop_index = index
-            break
+    # plot parameters
+    plot_interval=1
 
     # plot
     os.makedirs('plots', exist_ok=True)
@@ -89,22 +96,24 @@ def plot_podnet(batch, index_within_batch):
     plt.xlabel('Time Steps')
     plt.ylabel('Position')
     plt.legend()
+    plt.grid()
     plt.tight_layout()
-    plt.savefig('plots/dynamics.png', dpi=300)
+    plt.savefig('plots/dynamics.png', dpi=600)
     #plt.show()
 
     plt.figure()
-    time = np.arange(0,stop_index,100)
-    c_t = c_t[0:stop_index:100]
-    plt.plot(time, np.argmax(c_t, axis=-1), 'ro')
+    time = np.arange(0,stop_index,plot_interval)
+    c_t = c_t[0:stop_index:plot_interval]
+    plt.plot(time, np.argmax(c_t, axis=-1), '.k', label='Predicted')
     plt.xlabel('Time Steps')
     plt.ylabel('Option')
     plt.legend()
+    plt.grid()
     plt.tight_layout()
-    plt.savefig('plots/options.png', dpi=300)
+    plt.savefig('plots/options.png', dpi=600)
     #plt.show()
 
-plot_podnet(0,0)
+plot_podnet(0,0, args.max_steps)
 
 
 # def hook_fn(m, i, o):
