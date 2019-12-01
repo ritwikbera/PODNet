minigrid dataset normalization, dataloader action dim fix for minigrid

data/split_csv.py
@@ -2,9 +2,17 @@ import numpy as np
 import os
 import pandas as pd
 
-input_file = 'minigrid20.csv'
+input_file = 'key_door_21epis.csv'
 output_dir = 'minigrid'
 
+def normalize(df):
+    features = ['x_t', 'y_t', 'heading_t']
+    result = df.copy()
+    for feature_name in features:
+        mean = np.mean(df[feature_name])
+        std = np.std(df[feature_name])
+        result[feature_name] = (df[feature_name]-mean)/std 
+    return result
 
 file = pd.read_csv(input_file)
 
@@ -19,6 +27,8 @@ new_header = ['episode', 't', 'x_t', 'y_t', 'heading_t', 'a_1']
 changes = dict(zip(old_header, new_header))
 
 file = file.rename(changes, axis=1)
+
+file = normalize(file)
 
 files = file.groupby('episode')
 print('Number of episodes {}'.format(files.ngroups))

utils.py
@@ -4,6 +4,7 @@ from torch.utils.data import Dataset
 import pandas as pd
 import glob
 import numpy as np
+from config import *
 from torch.utils.tensorboard import SummaryWriter
 
 def create_summary_writer(model, dataloader, log_dir):
@@ -49,7 +50,7 @@ class RoboDataset(Dataset):
         if self.root_dir == 'data/minigrid/':
             states = Tensor(np.array(traj.loc[:,'x_t':'a_1'])[:,:-1])
             actions = Tensor(np.array(traj.loc[:,'a_1':]))
-            action_dim = 3
+            action_dim = config('minigrid').action_dim
             actions = \
             torch.zeros(*actions.size()[:-1], action_dim).scatter_(-1, actions.type(torch.LongTensor), 1)
         