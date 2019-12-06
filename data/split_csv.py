fixed minigrid normalization, added focal loss, keypoints plotting in minigrid

data/split_csv.py
@@ -7,17 +7,21 @@ input_file = 'minigrid_keydoor18.csv'
 output_dir = 'minigrid'
 
 def normalize(df):
-    features = ['x_t', 'y_t', 'heading_t', 'key_x', 'key_y', 'goal_x', 'goal_y', 'door_x', 'door_y']
-    features = ['x_t', 'y_t', 'heading_t']
+    features = ['x_t', 'y_t', 'heading_t', 'key_x', 'key_y', 'door_x', 'door_y', 'goal_x', 'goal_y']
+    # features = ['x_t', 'y_t', 'heading_t']
     result = df.copy()
     # df['key_x'] = pd.to_numeric(df['key_x'])
+    position_feats = ['x_t', 'y_t', 'key_x', 'key_y', 'door_x', 'door_y', 'goal_x', 'goal_y']
+    pos_mean = np.ceil(df[position_feats].to_numpy().mean())
+    head_mean = np.ceil(np.mean(df['heading_t']))
+
+    print(pos_mean)
     print(df.dtypes)
-    for feature_name in features:
-        mean = np.ceil(np.mean(df[feature_name]))
-        print(mean)
-        std = np.std(df[feature_name])
-        std = 1
-        result[feature_name] = (df[feature_name]-mean)
+    print(head_mean)
+
+    std = 1
+    result[position_feats] = (df[position_feats]-pos_mean)
+    result['heading_t'] = (df['heading_t']-head_mean)
     return result
 
 file = pd.read_csv(input_file)

losses.py
@@ -8,11 +8,31 @@ def DynamicsLoss(next_state_segment, next_state_pred, PAD_TOKEN, device):
     return L_ODC
 
 def BCLoss(action_segment, action_pred, PAD_TOKEN, device, use_discrete=False):
-    mask = (action_segment!=PAD_TOKEN).type(torch.FloatTensor).to(device)
+    #focal loss parameters, borrowed from RetinaNet
+    alpha = 1.0 
+    gamma = 4
+
+    mask = (action_segment!=PAD_TOKEN).type(torch.LongTensor).to(device)
+    
+    #clean out PAD_TOKENS before entering action_segment into BinaryCrossEntropy
+    pad_mask = (1-mask).type(torch.BoolTensor)
+    action_segment.masked_fill_(pad_mask, 0)
+
+    # print(action_segment)
+    assert ((action_segment>=0).all()  and (action_segment<=1).all()) #unit test to ensure all inputs to BCE are >0, <1
+
     if use_discrete:
-        L_BC = (F.binary_cross_entropy(action_pred,action_segment,reduction='none')*mask).sum(-1).sum(-2).mean()
+        BCE_loss = F.binary_cross_entropy(action_pred,action_segment,reduction='none')
+        pt = torch.exp(-BCE_loss)
+        # print(pt) #print confidence scores
+        
+        F_loss = (alpha*(1-pt)**gamma)*BCE_loss
+        # F_loss = BCE_loss # Uncomment this line to disable focal loss
+        
+        L_BC = (F_loss*mask).sum(-1).sum(-2).mean()
     else:   
         L_BC = (((action_segment - action_pred)**2)*mask).sum(-1).sum(-2).mean()
+    
     return L_BC
 
 def KLDLoss(qy, mask, categorical_dim, device):